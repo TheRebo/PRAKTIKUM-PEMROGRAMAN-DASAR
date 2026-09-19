a50392bd32b70996e4ff22de1c5489a55b7107f2
Na'im Annafi Santosa <Naim_Annafi_Santosa@proton.me>
2026-09-19 18:30:35 +0800

    Add files via upload

---
+++ MODUL-0/C/PRAK002-2610817210026-Na'im_Annafi_Santosa.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main () {
+	printf("Selamat Pagi, Na'im Annafi Santosa\n"
+		   "Selamat Siang, Na'im Annafi Santosa\n"
+		   "Selamat Malam, Na'im Annafi Santosa");
+	return 0;
+}
